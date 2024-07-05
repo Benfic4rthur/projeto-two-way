4c4c488e66b6e6653e88a6a8b4c6ad103999b4b9
benfic4rthur <arthur_benfica@hotmail.com>
2024-07-05 14:41:56 -0300

    aprendendo two way databind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'projeto-two-way';
+  protected name: string = '';
+
+  protected handleInput(event: string) {
+    if (event) {
+      this.name = event;
+    }
+  }
 }
